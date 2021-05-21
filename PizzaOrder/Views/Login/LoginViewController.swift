6e3ffe4d9d3ac728f2c6d67417becc1c9cfa3d9e
Raphael de Oliveira Chagas <raphael.ochagas@gmail.com>
2021-05-20 22:30:25 -0300

    Refactor keyboard show methods in login view controller

--- PizzaOrder/Views/Login/LoginViewController.swift
+++ PizzaOrder/Views/Login/LoginViewController.swift
@@ -12,8 +12,15 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var topViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
     
     let viewModel: LoginViewModel
+    
+    var isKeyboardShow = false
     
     lazy var spinnerView = SpinnerView(forParent: view)
     
@@ -32,6 +39,10 @@ class LoginViewController: UIViewController {
         userTextfield.text = UserDefaults().string(forKey: Constants.Keys.loginUserDefaultsKey)
         loginButton.layer.cornerRadius = 10
         
+        userTextfield.delegate = self
+        passwordTextfield.delegate = self
+        
+        navigationController?.view.backgroundColor = .black
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide(_:)), name: UIResponder.keyboardDidHideNotification, object: nil)
     }
@@ -68,23 +79,57 @@ class LoginViewController: UIViewController {
     
     @objc
     func keyboardDidShow(_ notification: Notification) {
-        guard let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+        guard let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue, !isKeyboardShow else {
             view.frame.origin.y = 0
             return
         }
         
+        isKeyboardShow = true
+        
         let keyboardRectangle = keyboardFrame.cgRectValue
         let keyboardHeight = keyboardRectangle.height
+        let loginButtonBottomY = loginButton.frame.origin.y + loginButton.frame.height + 12
+        let distance = abs(loginButtonBottomY - view.frame.height)
         
         UIView.animate(withDuration: 0.25) {
-            self.view.frame.origin.y = -(keyboardHeight - 60)
+            
+            self.imageViewLeadingConstraint.constant -= 31
+            self.imageViewTrailingConstraint.constant -= 31
+            self.imageViewBottomConstraint.constant -= 31
+            self.imageViewTopConstraint.constant -= 31
+            self.topViewHeightConstraint.constant -= (keyboardHeight - distance)
+            self.view.layoutIfNeeded()
         }
     }
     
     @objc
     func keyboardDidHide(_ notification: Notification) {
+        guard isKeyboardShow else { return }
+        isKeyboardShow = false
+        
         UIView.animate(withDuration: 0.25) {
-            self.view.frame.origin.y = 0
+            self.imageViewLeadingConstraint.constant += 31
+            self.imageViewTrailingConstraint.constant += 31
+            self.imageViewBottomConstraint.constant += 31
+            self.imageViewTopConstraint.constant += 31
+            self.topViewHeightConstraint.constant = 0
+            self.view.layoutIfNeeded()
         }
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField.returnKeyType {
+        case .next:
+            passwordTextfield.becomeFirstResponder()
+        case .send:
+            passwordTextfield.resignFirstResponder()
+            didTapLoginButton(loginButton as Any)
+        default:
+            return false
+        }
+        return true
     }
 }
